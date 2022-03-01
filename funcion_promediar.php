5123625d4869b9e312781ef95178f6b6b42ad705
abelorlandobenitez <abelorlandobenitez@gmail.com>
2022-03-01 15:31:22 -0300

    cambios

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -1,1 +1,19 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+function promediar($aNumeros){
+    $suma = 0;
+    foreach($aNumeros as $numero){
+
+        $suma += $numero;
+    }
+    return $suma / count($aNumeros);
+
+
+}
+
+
